0ea73f50f96856b472f6fff66bb202d01c174097
Davyduty <129392472+Davyduty@users.noreply.github.com>
2023-10-24 18:32:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-global.owner = ['6281395861695']  
-global.mods = ['6281395861695'] 
-global.prems = ['6281395861695']
-global.nameowner = 'Tio'
-global.numberowner = '6281395861695' 
+global.owner = ['0717442167']  
+global.mods = ['0717442167'] 
+global.prems = ['0717442167']
+global.nameowner = 'Davyduty'
+global.numberowner = '0717442167' 
 global.mail = 'support@tioprm.my.id' 
 global.gc = 'https://chat.whatsapp.com/Ln2vHjRrRayAbzalRMB56r'
 global.instagram = 'https://instagram.com/prm2.0'
-global.wm = '© Tio'
+global.wm = '© Davyduty'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made With'
-global.author = 'Bot WhatsApp'
+global.author = 'Davyduty'
 
 //INI WAJIB DI ISI!//
 global.btc = 'YOUR_APIKEY_HERE' 
